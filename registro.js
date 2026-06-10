6e49d53e7c0d91c64d5234f8f4f40aa35f400a3f
vgonzalezaqro2024-maker <vgonzalezaqro.2024@gmail.com>
2026-06-10 14:40:49 -0600

    Update and rename script.js to registro.js

---
+++ registro.js
@@ -0,0 +1,7 @@
+
+function registrar(){
+localStorage.setItem('nombre',nombre.value);
+localStorage.setItem('edad',edad.value);
+localStorage.setItem('grado',grado.value);
+window.location='avatar.html';
+}
