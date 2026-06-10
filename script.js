9bf476f52e7784d9410bf4d12a4bbbec2ea1ea7b
vgonzalezaqro2024-maker <vgonzalezaqro.2024@gmail.com>
2026-06-10 12:27:57 -0600

    script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+function registrarJugador(){
+
+let nombre = document.getElementById("nombre").value;
+let edad = document.getElementById("edad").value;
+let grado = document.getElementById("grado").value;
+
+if(nombre==="" || edad==="" || grado===""){
+    alert("Completa todos los campos");
+    return;
+}
+
+localStorage.setItem("nombre",nombre);
+localStorage.setItem("edad",edad);
+localStorage.setItem("grado",grado);
+
+alert("¡Registro completado!");
+
+document.getElementById("registro").style.display="none";
+document.getElementById("menu").style.display="block";
+
+}
